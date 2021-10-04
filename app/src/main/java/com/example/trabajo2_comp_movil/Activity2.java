7dfa00f48b9a779b641b92491e72d6302f83ac6b
Diego Sepúlveda <diegosepu.soto@gmail.com>
2021-10-03 21:02:36 -0300

    agregando componentes en activity 2

--- app/src/main/java/com/example/trabajo2_comp_movil/Activity2.java
+++ app/src/main/java/com/example/trabajo2_comp_movil/Activity2.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.trabajo2_comp_movil.models.User;
 
 public class Activity2 extends AppCompatActivity {
+    Float seguridadContrasenia;
+
     Intent mainActivityIntent;
     TextView tvNombreUsuario;
     TextView tvCorreoUsuario;
     TextView tvContrasenia;
     TextView tvTipoUsuario;
+
+    ImageView imageTipoUsuario;
+
+    RatingBar ratingContrasenia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +34,26 @@ public class Activity2 extends AppCompatActivity {
         tvCorreoUsuario = findViewById(R.id.tvCorreoUsuario);
         tvContrasenia = findViewById(R.id.tvContrasenia);
         tvTipoUsuario = findViewById(R.id.tvTipoUsuario);
+        imageTipoUsuario = findViewById(R.id.imageTipoUsuario);
+        ratingContrasenia = findViewById(R.id.ratingContrasenia);
 
         tvNombreUsuario.setText(registeredUser.name);
         tvCorreoUsuario.setText(registeredUser.email);
         tvContrasenia.setText("Y tu contraseña tiene " + registeredUser.password.length() + " caracteres");
-        tvTipoUsuario.setText("Es un: " + registeredUser.role);
+        tvTipoUsuario.setText("Eres un: " + registeredUser.role);
+
+        if(registeredUser.password.length() >= 8) {
+            seguridadContrasenia = (float) 8.0;
+        } else {
+            seguridadContrasenia = (float) registeredUser.password.length();
+        }
+
+        ratingContrasenia.setRating(seguridadContrasenia);
+
+        if (registeredUser.role.equals("Administrador")) {
+            imageTipoUsuario.setImageResource(R.drawable.admin);
+        } else {
+            imageTipoUsuario.setImageResource(R.drawable.user);
+        }
     }
 }
